45ddac675d0353a8bb59b79f57cf74d1cd3cfb48
Karen Angela Uy <karenangelauy@Karens-MacBook-Pro.local>
2017-11-07 11:29:03 +0800

    added .gitignore .gitattributtes and TipCalculator

--- TipCalculator/ViewController.swift
+++ TipCalculator/ViewController.swift
@@ -10,6 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var Billfield: UITextField!
+    @IBOutlet weak var Tiplabel: UILabel!
+    @IBOutlet weak var Totallabel: UILabel!
+    @IBOutlet weak var Tipcontrol: UISegmentedControl!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +27,24 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func onTap(_ sender: Any) {
+        view.endEditing(true)
+    }
+    
+    @IBAction func calculatetip(_ sender: Any) {
+   
+        let Tippercentages = [0.1, 0.2, 0.3]
+        
+        let Bill = Double(Billfield.text!) ?? 0
+        let Tip = Bill * Tippercentages[Tipcontrol.selectedSegmentIndex]
+        let Total = Bill + Tip
+        
+        
+        Tiplabel.text = String(format: "$%.2f", Tip)
+        Totallabel.text = String(format: "$%.2f", Total)
+    
+    
+    }
+    
 }
 
